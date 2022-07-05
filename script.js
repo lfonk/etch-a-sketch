44c99e98852c2f7c626e1313ada4a72ac5e9ff0e
lfonk <jsharp4212@gmail.com>
2022-07-05 17:54:53 +0800

    Add rainbow square colors

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const title = document.createElement("h1")
+title.textContent = "Etch-a-Sketch"
+document.body.appendChild(title)
+
 const container = document.createElement("div")
 container.classList.add(`container`)
 document.body.appendChild(container);
@@ -16,6 +20,11 @@ resetButton.textContent="Reset"
 resetButton.classList.add("button")
 buttonCon.appendChild(resetButton)
 
+let rainbow = document.createElement('button')
+rainbow.textContent ="Rainbow Mode"
+rainbow.classList.add('rainbow')
+buttonCon.appendChild(rainbow)
+
 const gridDim = document.createElement("div")
 gridDim.classList.add(`gridDim`)
 container.appendChild(gridDim);
@@ -26,13 +35,13 @@ start.addEventListener('click', function() {
     gridDimension = prompt("try again", "")
   }
   document.documentElement.style.setProperty('--grid-dimension',gridDimension)
-  
+  let color = "black"
   for (let i = 1; i <= (gridDimension*gridDimension); i ++) {
     const square = document.createElement("div")
     square.classList.add(`square`)
     gridDim.appendChild(square)
     square.addEventListener("mouseover", function( e ) {
-      e.target.style.backgroundColor = "black";
+      e.target.style.backgroundColor = color;
     });
   }
 });
@@ -41,6 +50,29 @@ resetButton.addEventListener('click', () => {
   window.location.reload();
 })
 
+
+rainbow.addEventListener('click', function() {
+  gridDimension = prompt("How many squares for each side?","")
+  if (gridDimension > 100) {
+    gridDimension = prompt("try again", "")
+  }
+  document.documentElement.style.setProperty('--grid-dimension',gridDimension)
+  for (let i = 1; i <= (gridDimension*gridDimension); i ++) {
+    const square = document.createElement("div")
+    square.classList.add(`square`)
+    gridDim.appendChild(square)
+    square.addEventListener("mouseover", function( e ) {
+      e.target.style.backgroundColor = `${random_bg_color()}`;
+    });
+  }
+});
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = 100+ Math.floor(Math.random() * 256);
+    var z = 50+ Math.floor(Math.random() * 256);
+    return "rgb(" + x + "," + y + "," + z + ")";
+}
 
 
 
