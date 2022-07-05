94ba059bcd4dd74c9bdb70912ec7ac60465d44c4
lfonk <jsharp4212@gmail.com>
2022-07-05 17:19:38 +0800

    Add reset function

--- script.js
+++ script.js
@@ -2,12 +2,46 @@ const container = document.createElement("div")
 container.classList.add(`container`)
 document.body.appendChild(container);
 
-for (let i = 1; i <= 256; i ++) {
-  const square = document.createElement("div")
-  square.classList.add(`square`)
-  container.appendChild(square)
-  square.addEventListener("mouseover", function( e ) {
-    e.target.style.backgroundColor = "black";
-  });
-}
+const buttonCon = document.createElement("div")
+buttonCon.classList.add(`buttonCon`)
+container.appendChild(buttonCon);
+
+const start = document.createElement("button")
+start.textContent="Start"
+start.classList.add("button")
+buttonCon.appendChild(start)
+
+const resetButton = document.createElement("button")
+resetButton.textContent="Reset"
+resetButton.classList.add("button")
+buttonCon.appendChild(resetButton)
+
+const gridDim = document.createElement("div")
+gridDim.classList.add(`gridDim`)
+container.appendChild(gridDim);
+
+start.addEventListener('click', function() {
+  gridDimension = prompt("How many squares for each side?","")
+  if (gridDimension > 100) {
+    gridDimension = prompt("try again", "")
+  }
+  document.documentElement.style.setProperty('--grid-dimension',gridDimension)
+  
+  for (let i = 1; i <= (gridDimension*gridDimension); i ++) {
+    const square = document.createElement("div")
+    square.classList.add(`square`)
+    gridDim.appendChild(square)
+    square.addEventListener("mouseover", function( e ) {
+      e.target.style.backgroundColor = "black";
+    });
+  }
+});
+
+resetButton.addEventListener('click', () => {
+  window.location.reload();
+})
+
+
+
+
 
