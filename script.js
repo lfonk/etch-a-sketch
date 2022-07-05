cf52f3651ec6184c5bfc63ed5d790747b33b9349
lfonk <jsharp4212@gmail.com>
2022-07-05 15:59:25 +0800

    Add mouseover function

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ container.classList.add(`container`)
 document.body.appendChild(container);
 
 for (let i = 1; i <= 256; i ++) {
-    const square = document.createElement("div")
-    square.classList.add(`square`)
-    container.appendChild(square)
+  const square = document.createElement("div")
+  square.classList.add(`square`)
+  container.appendChild(square)
+  square.addEventListener("mouseover", function( e ) {
+    e.target.style.backgroundColor = "black";
+  });
 }
-
 
